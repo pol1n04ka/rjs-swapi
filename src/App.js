d9e7416008ec8933233780a1155e47ec811fc348
Polina Prokopenko <topolina1337@gmail.com>
2023-03-07 17:54:15 +0100

    base project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import styles from "./App.module.css";
+
+import { useCallback, useEffect, useState } from "react";
+import Container from "./components/layout/Container";
+import Card from "./components/UI/Card";
 
 function App() {
+  const [movies, setMovies] = useState({});
+
+  const fetchMoviesHandler = useCallback(async () => {
+    try {
+      const response = await fetch("https://swapi.py4e.com/api/films/");
+      setMovies(await response.json());
+    } catch {
+      throw new Error();
+    }
+  }, []);
+
+  useEffect(() => {
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler]);
+
+  useEffect(() => {
+    console.log(movies);
+  }, [movies]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.app}>
+      <Container>
+        <h2>Hi! It's my simple React.js app which using Star Wars API</h2>
+        <Card />
+      </Container>
     </div>
   );
 }
